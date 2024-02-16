8dd3bc9f0a1ce83af0c7381240fab21ab7125cab
zayang <zayang@paypal.com>
2024-02-16 15:44:34 -0800

    adding lodash into dependencies no longer works, it does not gets deployed as a ecmascript module in the instance

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
 import { GlideRecord, gs } from "@servicenow/glide";
-// import * as lodash from 'lodash'
+import * as lodash from 'lodash'
 
 export const addLog = (newValue: string, oldValue: string) => {
     gs.addInfoMessage(`Value changed from  ${oldValue} to ${newValue}`);
-    // gs.addErrorMessage(`Logging with Lodash: ${lodash.toUpper(newValue)}`)
+    gs.addErrorMessage(`Logging with Lodash: ${lodash.toUpper(newValue)}`)
 }
