61af22ed065ce65986b24441fd39bba2ba5ec421
zayang <zayang@paypal.com>
2024-02-16 15:42:00 -0800

    now-sdk correcly deploys without lodash

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,7 @@
 import { GlideRecord, gs } from "@servicenow/glide";
 // import * as lodash from 'lodash'
 
-export const addLog = (value: string) => {
-    // gs.addErrorMessage(`Category changed from "${_.toUpper(_prev.getDisplayValue('category'))}" to "${_.toUpper(_curr.getDisplayValue('category'))}" !`);
-    gs.addInfoMessage(`The value is now ${value}`);
-    // gs.addErrorMessage(`The value is now ${lodash.toUpper(value)}`)
+export const addLog = (newValue: string, oldValue: string) => {
+    gs.addInfoMessage(`Value changed from  ${oldValue} to ${newValue}`);
+    // gs.addErrorMessage(`Logging with Lodash: ${lodash.toUpper(newValue)}`)
 }
