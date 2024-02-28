90746e8b732f2667bdef81f85086f32011b4800f
zayang <zayang@paypal.com>
2024-02-28 15:29:09 -0800

    fixed tsconfig.json

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,11 @@
 import { GlideRecord, gs } from "@servicenow/glide"
 import * as _ from 'lodash'
 import * as moment from 'moment'
+// import { format } from 'date-fns'
 
 export const addLog = (newValue: string, oldValue: string) => {
     let datestr = moment(moment(), 'LLLL');
+    // let datestr = format(new Date(), 'LLLL');
     let msg = `Logging with Lodash. Value changed from  ${_.toUpper(oldValue)} to ${_.toUpper(newValue)} on ${datestr}`;
     gs.addErrorMessage(msg);
 }
